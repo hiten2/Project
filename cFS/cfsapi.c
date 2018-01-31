27e91b6e7169635887cd93af16e52444bd2a39a9
hiten2 <36002061+hiten2@users.noreply.github.com>
2018-01-31 18:11:05 -0500

    Update cfsapi.c

--- cFS/cfsapi.c
+++ cFS/cfsapi.c
@@ -3,8 +3,8 @@
 #include "cfsapi.h"
 
 /* create file *//* not done */
-int cfs_create_file(cFS *cfs, char *path) {
-  if (cfs != NULL && path != NULL) {
+int cfs_create_file(struct cFS *cfs, char *path) {
+  if (cfs != NULL && cfs->node != NULL && path != NULL) {
     /* pass */
     return 1;
   }
@@ -12,13 +12,13 @@ int cfs_create_file(cFS *cfs, char *path) {
 }
 
 /* read bytes from file *//* not done */
-int cfs_read(cFS *cfs, char *buffer, char *path, int lim);
+int cfs_read(struct cFS *cfs, char *buffer, char *path, int lim);
 
 /* remove file *//* not done */
-int cfs_remove_file(cFS *cfs, char *path);
+int cfs_remove_file(struct cFS *cfs, char *path);
 
 /* shred & remove file *//* not done */
-int cfs_sremove_file(cFS *cfs, char *path);
+int cfs_sremove_file(struct cFS *cfs, char *path);
 
 /* write bytes to file *//* not done */
-int cfs_write(cFS *cfs, char *path, char *buffer, int lim);
+int cfs_write(struct cFS *cfs, char *path, char *buffer, int lim);
