2362d6181f73bc0abdcad2427b16d159547f5593
hiten2 <36002061+hiten2@users.noreply.github.com>
2018-01-31 17:57:46 -0500

    Update cfs.c

--- cFS/cfs.c
+++ cFS/cfs.c
@@ -1,1 +1,18 @@
+/* cFS */
 
+#include "cfs.h"
+
+/* get cFS long long size */
+int cfs_get_long_long_size(char *nodePath) {
+  cFS *node;
+  int size;
+  size = 0;
+  
+  if (nodePath != NULL && (node = fopen(nodePath, "rb")) != NULL) {
+    char buffer[1];
+    fread(buffer, 1, 1, node);
+    fclose(node);
+    size = (int) buffer[0];
+  }
+  return size;
+}
