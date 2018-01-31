348cf2188e9b6779c8a860c121375164d9c574e3
hiten2 <36002061+hiten2@users.noreply.github.com>
2018-01-31 18:44:03 -0500

    Update cfsapi.c

--- cFS/cfsapi.c
+++ cFS/cfsapi.c
@@ -11,6 +11,26 @@ int cfs_create_file(struct cFS *cfs, char *path) {
   return 0;
 }
 
+/* destroy cFS *//* not done */
+int cfs_destroy(struct cFS *cfs);
+
+/* make cFS at the given path *//* not done */
+int cfs_make(struct cFS *cfs) {
+  FILE *node;
+  unsigned long size;
+  
+  if ((node = fopen(nodePath, "wb")) != NULL && (size = get_fsize(node)) > CFS_MIN_STORAGE_SIZE) {
+    write((char) sizeof(long long), 1, node); /* byte 1 = size of size block */
+    write(size, sizeof(long long), 1, node); /*bytes 2-size of size block = size of storage medium */
+    write(key_size, 1, sizeof(long long), node);
+    int i;
+    
+    for (i = 0; i < CFS_LOCK_SIZE; write(i++ % 512, 1, 1, node));
+    
+    for (i = 0; i < CFS_TREEMAP_SIZE; write('\0', 1, 1, node), i++); /* initialize tree mapping portion */
+  }
+}
+
 /* read bytes from file *//* not done */
 int cfs_read(struct cFS *cfs, char *buffer, char *path, int lim);
 
