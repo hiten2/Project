245a83e48f7d375f3fbaccd3c0dedf1ee0dce16c
hiten2 <36002061+hiten2@users.noreply.github.com>
2018-01-31 18:38:48 -0500

    Update mkfs.cfs.c

--- cFS/mkfs.cfs.c
+++ cFS/mkfs.cfs.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "cfs.h"
+#include "cfsapi.h"
 
 /* mkfs utility for cFS */
 
@@ -17,22 +18,6 @@ unsigned long getfsize(FILE *fp) {
 
 void main(int argc, char *argv[]) {
   if (argc > 1) {
-    for (int i = 0; i < argc; mkfs_cfs(argv[i++]));
+    for (int i = 0; i < argc; make_cfs(argv[i++]));
   }
-}
-
-/* make cFS *//* not done */
-int mkfs_cfs(char *nodePath) {
-  FILE *node;
-  unsigned long size;
-  
-  if ((node = fopen(nodePath, "wb")) != NULL && (size = get_fsize(node)) > CFS_MIN_STORAGE_SIZE) {
-    fwrite((char) sizeof(long long), 1, node); /* byte 1 = size of size block */
-    fwrite(size, sizeof(long long), 1, node); /*bytes 2-size of size block = size of storage medium */
-    fwrite(key_size, 1, sizeof(long long), node);
-    int i;
-    
-    for (i = 0; i < CFS_LOCK_SIZE; fwrite(i++ % 512, 1, 1, node));
-    
-    for (i = 0; i < CFS_TREEMAP_SIZE; fwrite('\0', 1, 1, node), i++); /* initialize tree mapping portion */
 }
