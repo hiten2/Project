9332b18b360c6ea6ff1469685097c864c66af962
hiten2 <36002061+hiten2@users.noreply.github.com>
2018-01-31 18:09:47 -0500

    struct cFS
    added a structure representation of cFS to include key portion

--- cFS/cfsapi.h
+++ cFS/cfsapi.h
@@ -6,29 +6,34 @@
 #ifndef CFSAPI_H_
 #define CFSAPI_H_
 
-typedef FILE cFS;
-
 /* close cFS */
 #undef cfs_close
-#define cfs_close(cfs) fclose(cfs)
+#define cfs_close(cfs) fclose(cfs->node)
 
 /* open cFS */
 #undef cfs_open
 #define cfs_open(nodePath) fopen(nodePath, "r+")
 
+/* cFS outline */
+struct cFS {
+  char *key;
+  long keySize;
+  FILE *node;
+};
+
 /* create file */
-int cfs_create_file(cFS *cfs, char *path);
+int cfs_create_file(struct cFS *cfs, char *path);
 
 /* read bytes from file */
-int cfs_read(cFS *cfs, char *buffer, char *path, int lim);
+int cfs_read(struct cFS *cfs, char *buffer, char *path, int lim);
 
 /* remove file */
-int cfs_remove_file(cFS *cfs, char *path);
+int cfs_remove_file(struct cFS *cfs, char *path);
 
 /* shred & remove file */
-int cfs_sremove_file(cFS *cfs, char *path);
+int cfs_sremove_file(struct cFS *cfs, char *path);
 
 /* write bytes to file */
-int cfs_write(cFS *cfs, char *path, char *buffer, int lim);
+int cfs_write(struct cFS *cfs, char *path, char *buffer, int lim);
 
 #endif
