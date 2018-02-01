448caf2c35681d531dad4b8ba881a16caa921c5e
hiten2 <36002061+hiten2@users.noreply.github.com>
2018-01-31 19:02:25 -0500

    Update mkfs.cfs.c

--- cFS/mkfs.cfs.c
+++ cFS/mkfs.cfs.c
@@ -1,23 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "cfs.h"
 #include "cfsapi.h"
 
 /* mkfs utility for cFS */
 
-/* return file size */
-unsigned long getfsize(FILE *fp) {
-  if (fp != NULL) {
-    unsigned long int cur = ftell(fp);
-    fseek(fp, 0, SEEK_END);
-    size = ftell(fp);
-    fseek(fp, cur, SEEK_SET);
-  }
-  return 0;
-}
-
 void main(int argc, char *argv[]) {
   if (argc > 1) {
-    for (int i = 0; i < argc; cfs_make(argv[i++]));
+    for (int i = 0; i < argc; cfs_make(argv[i++]))
+      printf("Making cFS on %s....\n", argv[i]);
   }
 }
