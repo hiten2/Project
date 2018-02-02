1f8b452356cda3fac6c11092d547bcda5ab09d12
hiten2 <36002061+hiten2@users.noreply.github.com>
2018-02-02 15:20:26 -0500

    Update diskdll.py

--- cFS/diskdll.py
+++ cFS/diskdll.py
@@ -1,1 +1,32 @@
+"""doubly-linked list residing on secondary storage"""
 
+import os
+import sys
+
+sys.path.append(os.path.realpath(__file__))
+
+import .diskutil as diskutil # methods for encoding ints and longs as strings
+
+class DiskDLL:
+  LONG_LONG_SIZE = diskutil.
+  
+  def __init__(self, pos):
+    self.pos = pos # pointer to "entry" node
+    
+    # position indices
+    self._cur = -1L
+    self._next = -1L
+    self._prev = -1L
+  
+  def next(self):
+    """return string encapsulated by next node and increase pointer"""
+    assert not self._next == -1, "reached end of list"
+  
+  def prev(self):
+    """return string encapsulated by previous node and decrease pointer"""
+    assert not self._prev == -1, "reached front of list"
+  
+  def reset(self):
+    """reset pointers"""
+    self._cur = self.pos
+    self._next = -1, self._prev = -1
