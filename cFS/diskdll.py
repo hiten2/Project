cf623a58f6ae7d2c42ba1c0d9d017bc63f00064f
hiten2 <36002061+hiten2@users.noreply.github.com>
2018-02-02 16:15:51 -0500

    implemented reading operations

--- cFS/diskdll.py
+++ cFS/diskdll.py
@@ -19,24 +19,56 @@ class DiskDLL:
   V                   V                                   V                   V
   [ previous address  |                data               |    next address   ]
   """
+  """so far only allows for reading"""
   
-  def __init__(self, pos):
+  def __init__(self, node, pos):
+    assert isintance(node, file) and not node.closed, "node must be an open file"
+    assert pos < 0, "pos must be >= 0"
+    self.node = node # file 
     self.pos = pos # pointer to "entry" node
     
-    # position indices
-    self._cur = -1L
-    self._next = -1L
-    self._prev = -1L
+    # current position index
+    self._cur = pos
   
   def next(self):
     """return string encapsulated by next node and increase pointer"""
-    assert not self._next == -1, "reached end of list"
+    assert self._cur >= 0, "reached end of list"
+    _, data, self._cur = self._parse_cur()
+    del _
+    return data
+  
+  def _parse_cur(self, quite = False):
+    """
+    return a tuple: ((long long) previous, (str) data, (long long) next)
+    enabling quiet surpresses I/O and OS errors
+    """
+    data = "", next = -1, prev = -1
+    assert not self.node.closed, "cannot read from closed node"
+    
+    try:
+      start = self.node.tell()
+      self.node.seek(self.pos, os.SEEK_SET)
+      prev = self.node.read(self.LONG_LONG_SIZE)
+      data = self.node.read(self.BLOCK_SIZE - (2 * self.LONG_LONG_SIZE))
+      next = self.node.read(self.LONG_LONG_SIZE)
+      self.node.seek(start, os.SEEK_SET)
+    except IOError as e:
+      if not quiet:
+        raise e
+    except OSError as e:
+      if not quiet:
+        raise e
+    except Exception as e:
+      raise e # raise all other exceptions
+    return diskutil.atol(prev), data, diskutil.atol(next)
   
   def prev(self):
     """return string encapsulated by previous node and decrease pointer"""
-    assert not self._prev == -1, "reached front of list"
+    assert self._cur >= 0, "reached front of list"
+    _, data, self._cur = self._parse_cur()
+    del _
+    return data
   
   def reset(self):
     """reset pointers"""
     self._cur = self.pos
-    self._next = -1, self._prev = -1
