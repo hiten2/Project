19de8d4dd95b83353c6eea5c9dc04f26958f84b9
hiten2 <36002061+hiten2@users.noreply.github.com>
2018-02-02 20:27:24 -0500

    Create diskutil.py

---
+++ cFS/diskutil.py
@@ -0,0 +1,12 @@
+"""disk representation/interpretation for long longs"""
+
+global LONG_LONG_SIZE
+LONG_LONG_SIZE = 8
+
+def atol(a, bytes = LONG_LONG_SIZE):
+  """return a long long representation for a string"""
+  return
+
+def ltoa(l, bytes = LONG_LONG_SIZE):
+  """return a string representation for a long long"""
+  return
