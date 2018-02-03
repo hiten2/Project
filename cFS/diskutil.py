5b8d19244fc4dd5e53288ee210a012dc16a62955
hiten2 <36002061+hiten2@users.noreply.github.com>
2018-02-02 20:31:37 -0500

    Update diskutil.py

--- cFS/diskutil.py
+++ cFS/diskutil.py
@@ -1,11 +1,23 @@
 """disk representation/interpretation for long longs"""
 
+import os
+
 global LONG_LONG_SIZE
 LONG_LONG_SIZE = 8
 
 def atol(a, bytes = LONG_LONG_SIZE):
   """return a long long representation for a string"""
   return
+
+def get_file_size(fp):
+  if isinstance(fp, file) and not fp.closed:
+    try:
+      start = fp.tell()
+      fp.seek(0, os.SEEK_END)
+      size = fp.tell()
+      fp.seek(start, os.SEEK_SET)
+      return size
+  return 0
 
 def ltoa(l, bytes = LONG_LONG_SIZE):
   """return a string representation for a long long"""
