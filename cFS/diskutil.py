5e1a582273287142622b8088ce917814ae254ebb
hiten2 <36002061+hiten2@users.noreply.github.com>
2018-02-05 11:00:28 -0500

    Update diskutil.py

--- cFS/diskutil.py
+++ cFS/diskutil.py
@@ -1,28 +1,6 @@
-"""disk representation/interpretation for long longs"""
+"""disk stuff"""
 
 import os
-
-global LONG_LONG_SIZE
-LONG_LONG_SIZE = 8
-
-def atol(a):
-  """return a long long representation for a string"""
-  l = 0L
-  
-  # convert to little endian first
-  # so conversion can be done in a cleaner iterative fashion
-  bytes = []
-  
-  for c in a:
-    bytes.append(bin(ord(c))[2:][::-1])
-  i = 0
-  
-  for byte in range(len(bytes) - 1, -1, -1):
-    for bit in bytes[byte]:
-      if bit:
-        l += ((long) 2) ** bit # ensure long precision
-      i += 1
-  return l
 
 def get_file_size(fp, quiet = False):
   """return file size, optionally surpress I/O and OS errors"""
@@ -42,17 +20,3 @@ def get_file_size(fp, quiet = False):
     except Exception as e:
       raise e
   return 0
-
-def ltoa(l):
-  """return a string representation for a long long"""
-  a = ""
-  bytes = []
-  
-  while l > 0:
-    bytes.insert(0, 0)
-    
-    for i in range(8):
-      bytes[0] += (l % 2) * (2 ** i)
-      l /= 2
-  a = "".join((chr(b) for b in bytes))
-  return a
