32cbf78d96eddf8edd7e776e652677777e2b8ce1
hiten2 <36002061+hiten2@users.noreply.github.com>
2018-02-14 12:50:38 -0500

    Update cfstest.py

--- cfstest.py
+++ cfstest.py
@@ -1,4 +1,14 @@
 """testing"""
 
+import cfs
+import sys
+
 if __name__ == "__main__":
-    import cfs
+
+    print "cfs.longs\n"
+    
+    print "cfs.longs.LONG_SIZE:", cfs.longs.LONG_SIZE
+    i = sys.maxint + 1
+    a = cfs.longs.ltoa(i)
+    print "cfs.longs.ltoa(%s):" % str(i), [a]
+    print "cfs.longs.atol(cfs.longs.ltoa(%s)):" % str(i), cfs.longs.atol(a)
