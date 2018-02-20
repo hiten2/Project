746c7d41d3554125f4d29c634e0723c8a524f7e7
hiten2 <hiten@riseup.net>
2018-02-20 15:33:16 -0500

    implemented preliminary inode structure

--- cFS/longs.py
+++ cFS/longs.py
@@ -7,23 +7,23 @@ import sys
 global LONG_SIZE # Python longs have infinite precision, so this is a generalization
 
 def atol(a):
-  """return a long long representation for a string"""
-  a = [ord(c) for c in a]
-  a.reverse()
-  l = 0L
+    """return a long long representation for a string"""
+    a = [ord(c) for c in a]
+    a.reverse()
+    l = 0L
 
-  for i, d in enumerate(a):
-    l += d * (256 ** i)
-  return l
+    for i, d in enumerate(a):
+        l += d * (256 ** i)
+    return l
 
 def ltoa(l):
-  """return a string representation for a long long"""
-  a = []
-  
-  while l > 0:
-    a.append(chr(l % 256))
-    l /= 256
-  a.reverse()
-  return "".join(a)
+    """return a string representation for a long long"""
+    a = []
+    
+    while l > 0:
+        a.append(chr(l % 256))
+        l /= 256
+    a.reverse()
+    return "".join(a)
 
 LONG_SIZE = len(ltoa(sys.maxint + 1))
