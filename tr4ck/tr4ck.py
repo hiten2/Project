279a959b363cca945e8ba2d03d3b3d44e6801a61
hiten2 <hiten@riseup.net>
2018-06-16 18:18:44 -0400

    added print function

--- tr4ck/tr4ck.py
+++ tr4ck/tr4ck.py
@@ -1,8 +1,11 @@
 import os
-import scapy
+import scapy, scapy.all # scapy.all.sniff(....)
 import sys
 import uuid
 
 sys.path.append(os.path.realpath(__file__))
 
 import tr4ckdb
+
+def print_callback(*args, **kwargs):
+    print args, kwargs
---
+++ tr4ck/tr4ckdb.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+sys.path.append(os.path.realpath(__file__))
+
+import db
+
+class Tr4ckDB(DB):
+    """database for the tr4ck tool"""
+
+    pass
