430a7e846a5a6137aaa0d0292251223842bab33e
hiten2 <hiten@riseup.net>
2018-06-16 19:08:25 -0400

    outlined db wrapper

--- tr4ck/tr4ck.py
+++ tr4ck/tr4ck.py
@@ -7,7 +7,10 @@ sys.path.append(os.path.realpath(__file__))
 
 import tr4ckdb
 
+class Tr4ck:
+    
 def print_callback(*args, **kwargs):
+    print args[0].type
     print args, kwargs
 
 if __name__ == "__main__":
--- tr4ck/tr4ckdb.py
+++ tr4ck/tr4ckdb.py
@@ -3,9 +3,23 @@ import sys
 
 sys.path.append(os.path.realpath(__file__))
 
-#import db
+import db
 
-class Tr4ckDB:#(DB):
-    """database for the tr4ck tool"""
+class Tr4ckDB(db.DB):
+    """
+    database for the tr4ck tool
+    
+    packets are stored in the following format:
+    "source MAC->destination MAC"
+    """
 
-    pass
+    def __init__(self, *args, **kwargs):
+        db.DB.__init__(self, *args, **kwargs)
+
+    def _generate_id(self, packet):
+        """return the described ID for a packet"""
+        return str(packet)
+    
+    def store(self, packet, mode = "wb"):
+        """store the packet based on it addresses"""
+        db.DB.store(self, packet, self._generate_id(packet), mode = mode)
