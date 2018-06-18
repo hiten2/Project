974a4e82947c899683d9154b2607e94e3f80830d
hiten2 <hiten@riseup.net>
2018-06-17 22:57:15 -0400

    made storage optional

--- tr4ck/tr4ck.py
+++ tr4ck/tr4ck.py
@@ -15,10 +15,11 @@ class PacketTracker(tr4ckdb.MACDB):
     """
 
     def __init__(self, directory = os.getcwd(), db_mode = "ab",
-            concurrent = True, filter = lambda p: True):
+            concurrent = True, filter = lambda p: True, store = True):
         tr4ckdb.MACDB.__init__(self, directory, db_mode, concurrent)
 
         self._filter = filter
+        self._store = store
     
     def sniff(self):
         """sniff packets into the database"""
@@ -27,8 +28,10 @@ class PacketTracker(tr4ckdb.MACDB):
     def _sniffer_callback(self, packet):
         if self._filter(packet):
             id = self._generate_id(packet)
-            print id, "stored to",
-            print self._generate_path(id, self.store(packet))
+
+            if self._store:
+                print id, "stored to",
+                print self._generate_path(id, self.store(packet))
 
 if __name__ == "__main__":
-    PacketTracker("test").sniff() # test database creation
+    PacketTracker("test", store = False).sniff() # test database creation
