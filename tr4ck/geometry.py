c721d75c8eb50015f5f0b294e4320de2bc2a47cf
hiten2 <hiten@riseup.net>
2018-06-19 12:15:14 -0400

    attempted implementation of ray-casting algorithm

--- tr4ck/geometry.py
+++ tr4ck/geometry.py
@@ -42,16 +42,21 @@ class Point:
 
     def euclidean_distance(self, other):
         """compute the Euclidean distance from this point to another"""
-        diffs = [self.magnitudes[i] - other.magnitudes[i]
-            for i in range(len(self.magnitudes))]
+        diffs = [self[i] - other[i]
+            for i in range(len(self))]
         return math.sqrt(sum((d ** 2 for d in diffs)))
 
+    def __getitem__(self, index):
+        return self.magnitudes[index]
+
+    def __len__(self):
+        return len(self.magnitudes)
+    
     def slope(self, other):
         """return the 2D slope between the points"""
-        assert len(self.magnitudes) == 2 and len(other.magnitudes) == 2, \
-            "slope incalculable"
-        xdiff = other.magnitudes[1] - self.magnitudes[1]
-        ydiff = other.magnitudes[1] - self.magnitudes[1]
+        assert len(self) == 2 and len(other) == 2, "slope incalculable"
+        xdiff = other[1] - self[1]
+        ydiff = other[1] - self[1]
         return ydiff / float(xdiff)
 
     def __str__(self):
@@ -61,39 +66,70 @@ class Polygon:
     """representation for a 2D polygon"""
 
     def __init__(self, *vertices):
-        if not vertices:
-            vertices = ()
-        self.vertices = vertices
+        self.vertices = []
+        
+        for v in vertices: # ensure that all vertices are points
+            if not isinstance(v, Point):
+                v = Point(*v)
+            self.vertices.append(v)
+        self.vertices = tuple(self.vertices)
 
-    def __contains__(self, point):
-        pass
+    def __contains__(self, point):#############seems to work, but questionable
+        """ray casting algorithm for the point-in-polygon problem"""
+        nintersects = 0
+        ray_y = point[1]
+        
+        for i in range(len(self.vertices)):
+            a = self.vertices[i]
+            b = self.vertices[(i + 1) % len(self.vertices)] # allow wrap-around
+            
+            if max(a[0], b[0]) < point[0]: # only cast to the right
+                continue
+            
+            if ray_y >= min(a[1], b[1]) and ray_y <= max(a[1], b[1]):
+                try:
+                    m = a.slope(b)
+                except ZeroDivisionError: # vertical line
+                    nintersects += 1
+                    continue
+                leftmost = sorted((a, b), key = lambda p: p[0])[0]
+                eq_y_at_point = m * (point[0] - a[0]) + a[1]
+
+                if leftmost[1] <= ray_y and eq_y_at_point <= ray_y:
+                    nintersects += 1
+        return nintersects and nintersects % 2
 
     def normalize(self):
         """
         remove overlapping line segments by rearranging order
-        of vertices
-
+        of vertices as such:
+        1. split into two halves by Y-value
+        2. sort upper half by X-values, from least to greatest
+        3. sort lower half by X-values, from greatest to least
+        4. new vertices = upper half + lower half
+        
         the resulting polygon may not be congruent to the original
         """
         high = []
         low = []
         n_vertices = len(self.vertices)
-        mean_y = sum((v.magnitudes[1] for v in self.vertices))
+        mean_y = sum((v[1] for v in self.vertices))
         mean_y /= float(n_vertices)
         
         for v in self.vertices:
-            if v.magnitudes[1] > mean_y:
+            if v[1] > mean_y:
                 high.append(v)
             else:
                 low.append(v)
-        high = sorted(high, key = lambda v: v.magnitudes[0])
-        low = sorted(low, key = lambda v: v.magnitudes[0], reverse = True)
+        high = sorted(high, key = lambda v: v[0])
+        low = sorted(low, key = lambda v: v[0], reverse = True)
         self.vertices = high + low
 
     def __str__(self):
         return str(tuple((v.magnitudes for v in self.vertices)))
 
 if __name__ == "__main__": # testing
-    p = Polygon(Point(0, 0), Point(1, 0), Point(2, 2), Point(3, 0))
-    print p
-    print Point(2, 2) in p
+    p = Point(2, 2)
+    polygon = Polygon(Point(0, 0), Point(0, 1), Point(2, 2), Point(3, 1))
+    print p, "in", polygon, '?'
+    print p in polygon
