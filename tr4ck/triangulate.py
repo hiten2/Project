717d1e24f8e2fd5399ce85d00dd12ab4947beec1
hiten2 <hiten@riseup.net>
2018-06-23 19:09:01 -0400

    playing around ith kml

--- tr4ck/triangulate.py
+++ tr4ck/triangulate.py
@@ -12,13 +12,13 @@ __doc__ = """rudimentary triangulation"""
 def _polygon_to_kml(polygon, kmlcolor = "ff0000ff", outline = 0):###problematic
     """return the KML equivalent for a geometry.Polygon"""
     coords = [v[:2] + (0, ) for v in polygon.vertices] # lat./long. only
-    kml_coords = ''.join([','.join([str(e) for e in c]) + '.'
+    kml_coords = '\n' + '\n'.join([','.join([str(e) for e in c]) + '.' + '\n'
         for c in coords])
-    return lxml.etree.tostring(KML.kml(KML.Document(KML.Placemark(KML.Polygon(
-        KML.outerBoundaryIs(KML.linearRing(KML.Coordinates(kml_coords)))),
-            KML.Style(KML.PolyStyle(KML.color(kmlcolor),
-                KML.outline(outline)))))),
-        pretty_print = True)
+    return ("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+        + lxml.etree.tostring(KML.kml(KML.PlaceMark(KML.Polygon(KML.extrude(1),
+            KML.altitudeMode("relativeToGround"), KML.outerBoundaryIs(
+                KML.LinearRing(KML.coordinates(kml_coords)))))),
+        pretty_print = True))
 
 def triangulate(*circles):#####contingent upon geometry.Circle.intersect
     """triangulate a polygon based on a set of circles"""
@@ -29,8 +29,8 @@ def triangulate_to_kml(*circles):
     return _polygon_to_kml(triangulate(*circles))
 
 if __name__ == "__main__":
-    kml = _polygon_to_kml(geometry.Polygon((-100, -100), (-100, 100),
-        (100, 100), (100, -100)))
+    kml = _polygon_to_kml(geometry.Polygon((-1, -1), (-1, 1),
+        (1, 1), (1, -1)))
     print kml
     print >> open("triangulate-test.kml", "w"), kml
     print "Written to triangulate-test.kml"
