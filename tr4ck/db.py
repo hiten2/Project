a6622f7007bf4ddf05739c5cc5bda7164bf3643f
hiten2 <hiten@riseup.net>
2018-07-16 15:08:53 -0400

    db

--- tr4ck/db.py
+++ tr4ck/db.py
@@ -50,6 +50,7 @@ def _help():
           "\tinit\tinitialize the database (happens with all actions)\n" \
           "\tlist\tlist all entries as unicode-escaped strings\n" \
           "\tset NAME [DATA]\tset an entry\n" \
+          "\tsize\tprint the number of unique entries\n" \
           "NAME\n" \
           "\tan entry name (a CSV string)\n" \
           "DATA\n" \
@@ -171,6 +172,10 @@ class DB:
         """retrieve an entry"""
         with DBEntry(self._generate_path(name)) as entry:
             return entry.get()
+
+    def __len__(self):
+        """return the number of unique entries"""
+        return len(self.list())
     
     def list(self):
         """return a sorted list of all the entry names"""
@@ -328,7 +333,7 @@ if __name__ == "__main__":
         
         if action in ("append", "set") and len(sys.argv) > 4:
             data = sys.argv[4]
-    elif not action in ("clean", "init", "list"):
+    elif not action in ("clean", "init", "list", "size"):
         print "Invalid action."
         _help()
         sys.exit()
@@ -354,5 +359,7 @@ if __name__ == "__main__":
             sys.stdout.flush()
     elif action == "set":
         db[name] = data
+    elif action == "size":
+        print len(db)
     # otherwise action == "init"
     db.__exit__()
