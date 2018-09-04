179d63cfadd6f2a7899876c27f49c249ad3088fc
hiten2 <hiten@riseup.net>
2018-09-03 21:56:59 -0400

    added threading tests

--- threading/threading-tests.py
+++ threading/threading-tests.py
@@ -1,5 +1,6 @@
 import thread
 import threading
+import tkinter
 
 """
 thread.start_new_thread(entry_function, args = (), kwargs = {})
@@ -7,3 +8,11 @@ thread.start_new_thread(entry_function, args = (), kwargs = {})
 thread.exit()
     -> exits the current thread
 """
+
+def space_callback(event):
+    print 1
+
+window = tkinter.Tk()
+frame = tkinter.Frame(window, height = 100, width = 100)
+frame.bind("<Button1-Down>", space_callback)
+window.mainloop()
