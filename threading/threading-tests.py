826a3da22848725335456ed2922fa22524d89421
hiten2 <hiten@riseup.net>
2018-09-03 22:16:20 -0400

    added threading tests

--- threading/threading-tests.py
+++ threading/threading-tests.py
@@ -1,3 +1,4 @@
+import random
 import thread
 import threading
 import tkinter
@@ -9,12 +10,35 @@ thread.exit()
     -> exits the current thread
 """
 
-def callback(event):
-    print "event detected at", (event.x, event.y)
+class Callback:
+    """
+    a callable callback
+    so we can remember which widget the callback is for
+    """
+    
+    def __init__(self, widget):
+        self.widget = widget
+
+    def __call__(self, event):
+        print "event detected at", (event.x, event.y)
+
+class RandomBG(Callback):
+    """set a random background color for the widget"""
+    COLORS = ("red", "orange", "yellow", "green", "blue", "indigo", "violet")
+    
+    def __init__(self, *args, **kwargs):
+        Callback.__init__(self, *args, **kwargs)
+    
+    def __call__(self, event):
+        """execute the callback"""
+        Callback.__call__(self, event)
+        
+        new_bg = random.choice(self.COLORS)
+        self.widget.configure(background = new_bg)
+        print "setting background to", new_bg
 
 window = tkinter.Tk()
 frame = tkinter.Frame(window, height = 100, width = 100)
-frame.bind("<Button-1>", callback)
+frame.bind("<Button-1>", RandomBG(frame).__call__)
 frame.pack()
-
 window.mainloop()
