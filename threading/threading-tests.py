89bbcc92385c0f4176cf5f297c8eb40d498c8438
hiten2 <hiten@riseup.net>
2018-09-03 21:42:09 -0400

    added threading tests

--- threading/threading-tests.py
+++ threading/threading-tests.py
@@ -1,1 +1,9 @@
+import thread
+import threading
 
+"""
+thread.start_new_thread(entry_function, args = (), kwargs = {})
+    -> starts a new thread
+thread.exit()
+    -> exits the current thread
+"""
