e7ae44ef9470bfbe59fdfde41c8eecd2b041ff11
hiten2 <hiten@riseup.net>
2018-09-11 21:47:59 -0400

    updates

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2018 Bailey Defino
-# <http://hiten2.asuscomm.com>
+# <https://hiten2.github.io>
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
